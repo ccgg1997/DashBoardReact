2ff1dd5ffc997b61d9be37bbfd5d635c770516aa
JoseM-18 <91767929+JoseM-18@users.noreply.github.com>
2023-07-25 16:51:24 -0500

    generacion de productos apartir del cliente y con precios especiales realizado

--- src/components/Factura/auxComponents/FacturaInfoCliente/FacturaInfoCliente.jsx
+++ src/components/Factura/auxComponents/FacturaInfoCliente/FacturaInfoCliente.jsx
@@ -1,29 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./FacturaInfoCliente.css";
 import { useSelector } from "react-redux";
-import { useState, useEffect } from "react";
-import BasicTable from "../FacturaTable/FacturaTable";
 import { infoPreciosClienteEspecial } from "../../../Api/apiAddress";
+import BasicTable from "../FacturaTable/FacturaTable";
+import { useCallback } from "react";
+
 const FacturaInfo = () => {
+  // State variables
+  const [producto, setProductos] = useState([]);
+  const [preciosEspeciales, setPreciosEspeciales] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  // Redux state
   const clientes = useSelector((state) => state.clientes);
   const token = useSelector((state) => state.auth.token);
-
   const { cliente } = clientes;
 
-  const uniqueNames = [...new Set(cliente.map((item) => item.negocio))];
-  const [selected, setSelected] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
 
-  const [selectedItem, setSelectedItem] = useState("");
-
-    // Estado para almacenar la lista de productos
-    const [producto, setProductos] = useState([]);
-    const [preciosEspeciales, setPreciosEspeciales] = useState([]); 
-
-    // Función para actualizar el estado de productos
-    const handleProductosChange = (newProductos) => {
-      setProductos(newProductos);
-    };
-
+  // Date formatting
   const fechaActual = new Date().toLocaleDateString("es-ES", {
     weekday: "long",
     year: "numeric",
@@ -31,28 +26,62 @@ const FacturaInfo = () => {
     day: "numeric",
   });
 
-    useEffect(() => {
-    if (selected === "") {
+  // Calculate the total price
+  const updateTotal = useCallback(() => {
+    let total = 0;
+    producto.forEach((item) => {
+      total += item.precio * item.cantidad;
+    });
+    setTotal(total);
+  }, [producto]);
+
+  // Function to handle product changes
+  const handleProductosChange = useCallback(
+    (newProductos) => {
+      setProductos(newProductos);
+      // Dado que `setProductos` es asíncrono, podemos llamar a `updateTotal` en un callback
+      // para asegurarnos de que use el estado actualizado.
+    },
+    []
+  );
+
+  // Unique business names for dropdown
+  const uniqueNames = [...new Set(cliente.map((item) => item.negocio))];
+  const [selectedCliente, setSelectedCliente] = useState("");
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  // Fetch prices for selected client
+  const fetchPreciosEspeciales = async (selectedCliente, token) => {
+    try {
+      const preciosEspecialesData = await infoPreciosClienteEspecial(
+        selectedCliente,
+        token
+      );
+      setPreciosEspeciales(preciosEspecialesData);
+    } catch (error) {
+      console.error("Error al obtener los precios especiales:", error);
+    }
+  };
+
+  const handleCliente = async (e) => {
+    const selec = e.target.value;
+    console.log("selec", selec);
+    if (selec === "Selecciona un cliente") {
+      setSelectedCliente(selec);
+      setIsOpen(false);
       return;
     }
-    // Busca el objeto cliente correspondiente al valor seleccionado
-    const selectedCliente = cliente.find((item) => item.negocio === selected);
-    // Asigna el objeto cliente seleccionado a selectedItem
+    setIsOpen(true);
+    setSelectedCliente(selec);
+    // Find the selected client object
+    const selectedCliente = cliente.find((item) => item.negocio === selec);
     setSelectedItem(selectedCliente);
-
-    // Busca los precios especiales del cliente seleccionado
-    const fetchPreciosEspeciales = async () => {
-      try {
-        const preciosEspecialesData = await infoPreciosClienteEspecial(selectedCliente.id, token);
-        setPreciosEspeciales(preciosEspecialesData);
-      } catch (error) {
-        console.error("Error al obtener los precios especiales:", error);
-      }
-    };
-    console.log(preciosEspeciales);
-
-    fetchPreciosEspeciales();
-  }, [selected, cliente, token]);
+    console.log("selectedCliente", selectedCliente);
+    
+    // Fetch prices for the selected client
+    fetchPreciosEspeciales(selectedCliente.id, token);
+    setPreciosEspeciales(preciosEspeciales);
+  };
 
 
   return (
@@ -60,9 +89,11 @@ const FacturaInfo = () => {
       className="FacturaInfo"
       style={{ overflowY: "scroll", maxHeight: "450px" }}
     >
+      {/* Title */}
       <div className="FacturaInfo__title">
         <h1>Bolsas Romy</h1>
       </div>
+      {/* Information */}
       <div className="FacturaInfo__info">
         <div className="FacturaInfo__info__fecha">
           <p>{fechaActual}</p>
@@ -71,8 +102,8 @@ const FacturaInfo = () => {
           <p>
             Cliente:{" "}
             <select
-              value={selected}
-              onChange={(e) => setSelected(e.target.value)}
+              value={selectedCliente}
+              onChange={handleCliente}
             >
               <option>Selecciona un cliente</option>
               {uniqueNames.map((item) => (
@@ -83,12 +114,12 @@ const FacturaInfo = () => {
             </select>
           </p>
         </div>
+        {/* Selected client details */}
         {selectedItem && (
           <>
             <div className="FacturaInfo__info__duenio">
               <p>Dueño: {selectedItem.duenio}</p>
             </div>
-
             <div className="FacturaInfo__info__direccion">
               <p>Dirección: {selectedItem.direccion}</p>
             </div>
@@ -100,13 +131,19 @@ const FacturaInfo = () => {
             </div>
           </>
         )}
+        <div className="FacturaInfo__info__total">
+          {/* Total price */}
+          <p>Total: ${total}</p>
+        </div>
       </div>
+      {/* Product table */}
       <div className="FacturaInfo__table">
-        <BasicTable onProductosChange={handleProductosChange} />
+        {isOpen &&
+          <BasicTable onProductosChange={handleProductosChange} preciosEspeciales={preciosEspeciales} isSelected={selectedCliente} />
+        }
       </div>
     </div>
   );
 };
-
 
 export default FacturaInfo;
--- src/components/Factura/auxComponents/FacturaTable/FacturaTable.jsx
+++ src/components/Factura/auxComponents/FacturaTable/FacturaTable.jsx
@@ -9,9 +9,15 @@ import Paper from "@mui/material/Paper";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Notificacion from "../../../Basicos/Notificacion/Notificacion";
-import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 
-export default function BasicTable({onProductosChange}) {
+export default function BasicTable({ onProductosChange, preciosEspeciales, isSelected }) {
   const [newRow, setNewRow] = useState({
     product_id: "",
     nombre: "",
@@ -33,22 +39,18 @@ export default function BasicTable({onProductosChange}) {
   };
 
   const [rows, setRows] = useState([]);
-
   const productos = useSelector((state) => state.precios);
-
   const { precio } = productos;
-
   const uniqueCodes = [...new Set(precio.map((item) => item.product_id))];
-
   const [selected, setSelected] = useState("");
-
-  const [infoProduct, setInfoProduct] = useState('');
+  const [prod, setProd] = useState("");
 
   const handleClick = () => {
+    setRows([]);
     if (newRow.product_id === "") {
-      setMensaje("Selecciona un producto", "error")
+      setMensaje("Selecciona un producto", "error");
       return;
-    }else{
+    } else {
       setRows([...rows, newRow]);
       setSelected("");
       onProductosChange([...rows, newRow]);
@@ -57,26 +59,41 @@ export default function BasicTable({onProductosChange}) {
         nombre: "",
         precio: "",
         cantidad: 1,
-      }); 
+      });
     }
+  };
+
+  useEffect (() => {
+    if(isSelected === "Selecciona un cliente" ){
+      return;
+    } 
+      setRows([]);
+    onProductosChange([]);
+  },[isSelected])
+
+  const selectedProd = (row) => {
+    setProd(row);
+    handleOpen();
   };
 
   const handleOpen = () => {
     setIsModalOpen(true);
-  }
+  };
 
   const deleteRow = (index) => {
     const newRows = [...rows];
     newRows.splice(index, 1);
-    setRows(newRows);      
+    setRows(newRows);
     onProductosChange(newRows);
-
-  }
-
+  };
 
   useEffect(() => {
+    if(!preciosEspeciales){
+      alert("No hay precios especiales")
+      return;
+    }
     // Busca el objeto producto correspondiente al valor seleccionado
-    const selectedProducto = precio.find(
+    const selectedProducto = preciosEspeciales.find(
       (item) => item.product_id === selected
     );
     if (!selectedProducto) return;
@@ -98,72 +115,77 @@ export default function BasicTable({onProductosChange}) {
   }, [mostrarNotificacion]);
 
   return (
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">Código</TableCell>
-              <TableCell align="center">Cantidad</TableCell>
-              <TableCell align="center"></TableCell>
-              <TableCell align="center">Descripción</TableCell>
-              <TableCell align="center">Precio Unitario</TableCell>
-              <TableCell align="center">Total</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row, index) => (
-              <TableRow key={index}>
-                <TableCell align="center">{row.product_id}</TableCell>
-                <TableCell align="center">{row.cantidad}</TableCell>
-                <TableCell align="center"> <button onClick={handleOpen}>+</button></TableCell>
-                <TableCell align="center">{row.nombre}</TableCell>
-                <TableCell align="center">{row.precio}</TableCell>
-                <TableCell align="center">{row.total}</TableCell>
-                <TableCell align="center"><button onClick={() => deleteRow(index)}>Eliminar</button></TableCell>
-              </TableRow>
-            ))}
-            <TableRow>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Código</TableCell>
+            <TableCell align="center">Cantidad</TableCell>
+            <TableCell align="center"></TableCell>
+            <TableCell align="center">Descripción</TableCell>
+            <TableCell align="center">Precio Unitario</TableCell>
+            <TableCell align="center">Total</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, index) => (
+            <TableRow key={index}>
+              <TableCell align="center">{row.product_id}</TableCell>
+              <TableCell align="center">{row.cantidad}</TableCell>
               <TableCell align="center">
-                <select
-                  onChange={(e) => setSelected(e.target.value)}
-                  value={selected}
-                >
-                  <option value={""}>Selecciona un producto</option>
-                  {uniqueCodes.map((item) => (
-                    <option key={item} value={item}>
-                      {item}
-                    </option>
-                  ))}
-                </select>
+                {" "}
+                <button onClick={() => selectedProd(row)}>+</button>
               </TableCell>
+              <TableCell align="center">{row.nombre}</TableCell>
+              <TableCell align="center">{row.precio}</TableCell>
+              <TableCell align="center">{row.total}</TableCell>
               <TableCell align="center">
-                Nombre: {newRow ? newRow.nombre : ""}
-              </TableCell>
-              <TableCell align="center">
-                Precio: {newRow ? newRow.precio : ""}
+                <button onClick={() => deleteRow(index)}>Eliminar</button>
               </TableCell>
             </TableRow>
-          </TableBody>
-        </Table>
-        <button onClick={handleClick}>Agregar</button>
-        <Notificacion
+          ))}
+          <TableRow>
+            <TableCell align="center">
+              <select
+                onChange={(e) => setSelected(e.target.value)}
+                value={selected}
+              >
+                <option value={""}>Selecciona un producto</option>
+                {uniqueCodes.map((item) => (
+                  <option key={item} value={item}>
+                    {item}
+                  </option>
+                ))}
+              </select>
+            </TableCell>
+            <TableCell align="center">
+              Nombre: {newRow ? newRow.nombre : ""}
+            </TableCell>
+            <TableCell align="center">
+              Precio: {newRow ? newRow.precio : ""}
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+      <button onClick={handleClick}>Agregar</button>
+      <Notificacion
         mensaje={mensajeNotificacion}
         tipoNotificacion={tipoNotificacion}
         mostrarNotificacion={mostrarNotificacion}
       />
-      <DistribucionProducto isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} data = {infoProduct}/>
-      </TableContainer>
-
+      <DistribucionProducto
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        data={prod}
+      />
+    </TableContainer>
   );
 }
 
-const DistribucionProducto = ({isOpen, onClose,data}) => {
+const DistribucionProducto = ({ isOpen, onClose, data }) => {
   return (
     <div>
-      <Dialog
-        open={isOpen}
-        onClose={onClose}
-      >
+      <Dialog open={isOpen} onClose={onClose}>
         <DialogTitle>{"Distribución de productos"}</DialogTitle>
         <DialogContent>
           <DialogContentText>
@@ -171,6 +193,7 @@ const DistribucionProducto = ({isOpen, onClose,data}) => {
               <p>Producto: {data.nombre}</p>
               <p>Precio: {data.precio}</p>
               <p>Cantidad: {data.cantidad}</p>
+              <p>Total: {data.total}</p>
             </div>
           </DialogContentText>
         </DialogContent>
@@ -180,4 +203,4 @@ const DistribucionProducto = ({isOpen, onClose,data}) => {
       </Dialog>
     </div>
   );
-}
+};
